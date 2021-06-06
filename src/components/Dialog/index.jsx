99043933169bb4fc1611b96fb68fffe53f1f17a4
Reselim <notreselim@gmail.com>
2021-06-05 22:32:46 -0400

    Add Dialog component

---
+++ src/components/Dialog/index.jsx
@@ -0,0 +1,45 @@
+import React from "react"
+import ReactDOM from "react-dom"
+
+import TextButton from "/src/components/TextButton"
+
+import "./style.scss"
+
+function Dialog(props) {
+	return ReactDOM.createPortal((
+		<div className="dialog-container" onClick={props.onCancel}>
+			<div className="dialog">
+				{props.title ? <span className="dialog-title">{props.title}</span> : null}
+				{props.description ? <span className="dialog-description">{props.description}</span> : null}
+				{
+					props.children ? (
+						<div className="dialog-embed">
+							{props.children}
+						</div>
+					) : null
+				}
+				{
+					props.buttons ? (
+						<div className="dialog-buttons">
+							{
+								props.buttons.map((buttonData) => {
+									return (
+										<TextButton
+											key={buttonData.id}
+											text={buttonData.text}
+											style={buttonData.style}
+											variant={buttonData.variant}
+											onClick={buttonData.onClick}
+										/>
+									)
+								})
+							}
+						</div>
+					) : null
+				}
+			</div>
+		</div>
+	), document.body)
+}
+
+export default Dialog
