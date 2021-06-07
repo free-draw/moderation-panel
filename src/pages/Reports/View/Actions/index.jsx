b3c0fd2ee016a170f2ec6bb7a04e4ae0e5758145
Reselim <notreselim@gmail.com>
2021-06-07 16:42:00 -0400

    Split off report accept dialog into its own component

--- src/pages/Reports/View/Actions/index.jsx
+++ src/pages/Reports/View/Actions/index.jsx
@@ -1,11 +1,56 @@
 import React from "react"
+import { mdiAccountBoxMultipleOutline } from "@mdi/js"
 
 import Dialog from "/src/components/Dialog"
+import Dropdown from "/src/components/Dropdown"
 
 import AcceptIcon from "./accept-icon.svg"
 import DeclineIcon from "./decline-icon.svg"
 
 import "./style.scss"
+
+function ReportAcceptDialog(props) {
+	return (
+		<Dialog
+			title="Accept report"
+			buttons={[
+				{
+					id: "cancel",
+					text: "Cancel",
+					style: "flat",
+					onClick: props.close,
+				},
+				{
+					id: "confirm",
+					text: "Confirm",
+					style: "bordered",
+					onClick: () => {
+						props.accept()
+						props.close()
+					},
+				},
+			]}
+			onCancel={props.close}
+		>
+			<Dropdown
+				currentOption="test"
+				options={[
+					{
+						id: "test",
+						text: "wawa",
+						icon: mdiAccountBoxMultipleOutline,
+					},
+					{
+						id: "test2",
+						text: "wawaa",
+						icon: mdiAccountBoxMultipleOutline,
+					},
+				]}
+				onSelection={console.log}
+			/>
+		</Dialog>
+	)
+}
 
 function Actions(props) {
 	const report = props.report
@@ -18,26 +63,9 @@ function Actions(props) {
 				<AcceptIcon />
 				{
 					dialogOpen ? (
-						<Dialog
-							title="Accept report"
-							buttons={[
-								{
-									id: "cancel",
-									text: "Cancel",
-									style: "flat",
-									onClick: () => setDialogOpen(false),
-								},
-								{
-									id: "confirm",
-									text: "Confirm",
-									style: "bordered",
-									onClick: () => {
-										report.accept()
-										setDialogOpen(false)
-									},
-								},
-							]}
-							onCancel={() => setDialogOpen(false)}
+						<ReportAcceptDialog
+							close={() => setDialogOpen(false)}
+							accept={() => report.accept()}
 						/>
 					) : null
 				}
