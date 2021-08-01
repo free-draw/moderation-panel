656eae825d3ed8739bb3380983c957350b3425b4
Reselim <notreselim@gmail.com>
2021-08-01 05:29:20 -0400

    Add settings and current user to header

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,7 +1,11 @@
 import React from "react"
 import { Link, useRouteMatch } from "react-router-dom"
+import Icon from "@mdi/react"
+import { mdiCog } from "@mdi/js"
 
 import makeClassName from "/src/util/makeClassName"
+
+import { getCurrentUser } from "/src/api/auth"
 
 import logo from "url:/src/assets/logo.svg"
 
@@ -18,6 +22,16 @@ function HeaderNavigationButton(props) {
 }
 
 function Header() {
+	const [ loaded, setLoaded ] = React.useState(false)
+	const [ settingsOpen, setSettingsOpen ] = React.useState(false)
+	const [ username, setUsername ] = React.useState("")
+
+	React.useEffect(async () => {
+		const { type, moderator } = await getCurrentUser()
+		setUsername(type === "SERVER" ? "[SERVER]" : moderator.name)
+		setLoaded(true)
+	}, [])
+
 	return (
 		<div className="header">
 			<img src={logo} className="header-logo" />
@@ -26,6 +40,27 @@ function Header() {
 				<HeaderNavigationButton text="Reports" to="/reports" />
 				<HeaderNavigationButton text="Users" to="/users" />
 				<HeaderNavigationButton text="Logs" to="/logs" />
+			</div>
+			<div className="header-spacer" />
+			<div className="header-context">
+				{
+					loaded ? (
+						<span className="current-user">
+							Logged in as <em>{username}</em>
+						</span>
+					) : null
+				}
+				<div className="settings-container">
+					<Icon
+						className="settings-icon"
+						path={mdiCog}
+						size={1}
+						onClick={() => setSettingsOpen(!settingsOpen)}
+					/>
+					<div className={makeClassName("settings", { open: settingsOpen })}>
+						<span className="settings-notice">There's nothing here yet!</span>
+					</div>
+				</div>
 			</div>
 		</div>
 	)
