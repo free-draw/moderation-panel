2c155ca1c1829f5ce00c5ea0afdfafa946b51019
Reselim <notreselim@gmail.com>
2021-08-02 18:53:44 -0400

    Fix useReducedMotion

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,7 @@
 import React from "react"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { useReduceMotion } from "react-reduce-motion"
+import { Globals } from "react-spring"
 
 import Header from "./components/Header"
 
@@ -9,6 +11,14 @@ import Users from "./pages/Users"
 import Logs from "./pages/Logs"
 
 function App() {
+	const reducedMotion = useReduceMotion()
+
+	React.useEffect(() => {
+		Globals.assign({
+			skipAnimation: reducedMotion,
+		})
+	}, [ reducedMotion ])
+
 	return (
 		<BrowserRouter>
 			<Header />
--- src/main.js
+++ src/main.js
@@ -2,18 +2,11 @@ import * as PIXI from "pixi.js"
 import React from "react"
 import ReactDOM from "react-dom"
 
-import { useReduceMotion } from "react-reduce-motion"
-import { Globals } from "react-spring"
-
 import App from "./App"
 
 import "./style/base.scss"
 
 ReactDOM.render(<App />, document.getElementById("app"))
-
-Globals.assign({
-	skipAnimation: useReduceMotion(),
-})
 
 if (window.__PIXI_INSPECTOR_GLOBAL_HOOK__) {
 	window.__PIXI_INSPECTOR_GLOBAL_HOOK__.register({ PIXI })
