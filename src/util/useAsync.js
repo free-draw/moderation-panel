e9f9e9f9ad1b8570dc214c1f7d592d0a4d2a6826
Reselim <notreselim@gmail.com>
2021-08-04 22:45:14 -0400

    Add deferred parameter to useAsync

--- src/util/useAsync.js
+++ src/util/useAsync.js
@@ -1,12 +1,14 @@
 import React from "react"
 
-export default function useAsync(func) {
+export default function useAsync(func, deferred) {
 	return (...args) => {
 		const [ result, setResult ] = React.useState(null)
 		React.useEffect(async () => {
 			setResult(null)
-			setResult(await func(...args))
-		}, args)
+			if (!deferred) {
+				setResult(await func(...args))
+			}
+		}, [ deferred, ...args ])
 		return result
 	}
 }
