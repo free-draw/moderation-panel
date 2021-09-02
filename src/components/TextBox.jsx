c067cf64d1d1057799d32c8f3acdb4074c28b502
Reselim <notreselim@gmail.com>
2021-09-02 00:49:53 -0400

    Refactor TextBox

---
+++ src/components/TextBox.jsx
@@ -0,0 +1,28 @@
+import styled from "styled-components"
+
+import colors from "/src/presets/colors"
+
+const TextBoxElement = styled.input.attrs({
+	type: "text",
+})`
+	background: white;
+	color: black;
+	font-size: 16px;
+	font-weight: 400;
+	height: 42px;
+	width: 100%;
+	padding-left: 12px;
+	border: 1px solid ${colors.border};
+	border-radius: 8px;
+
+	&:placeholder {
+		color: rgba(0, 0, 0, 0.5);
+	}
+
+	&:focus-visible {
+		border: 1px solid ${colors.brand};
+		outline: none;
+	}
+`
+
+export default TextBoxElement
