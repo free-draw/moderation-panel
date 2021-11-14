7910a9dd9f23460f1edba12778b1d2632ea5737a
Reselim <notreselim@gmail.com>
2021-11-13 22:29:20 -0500

    Fix realtime location

--- src/Realtime.ts
+++ src/Realtime.ts
@@ -4,7 +4,7 @@ import Cookies from "js-cookie"
 const token = Cookies.get("token")
 if (!token) throw new Error("No token found")
 
-const realtime = new Realtime(window.location.toString(), token)
+const realtime = new Realtime(window.location.host, token)
 realtime.connect()
 
 export default realtime
