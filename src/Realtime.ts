418e601ffb6eaf9a58a4db51e03274b4e9231b9d
Reselim <notreselim@gmail.com>
2021-11-13 22:41:19 -0500

    Fix type issue

--- src/Realtime.ts
+++ src/Realtime.ts
@@ -2,7 +2,7 @@ import { Realtime } from "@free-draw/moderation-client"
 import Cookies from "js-cookie"
 
 const token = Cookies.get("token")
-const realtime = new Realtime(window.location.host, token)
+const realtime = new Realtime(window.location.host, token!)
 
 if (token) {
 	realtime.connect()
